445887b6e5012c73bcc04d273eca8a995adbcd7f
lastumber <www.514163307@qq.com>
2018-11-24 14:36:37 +0800

    添加 Json 处理类库, 添加文件Helper类

---
+++ IME_Test/FileHelper.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartInput
+{
+    public static class FileHelper
+    {
+        private readonly static string FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "SmartInput");
+        public static string FileName = "config.js";
+        public static string FilePath = Path.Combine(FolderPath, FileName);
+
+        public static void SaveJosn(object o)
+        {
+            string jsonString = JsonConvert.SerializeObject(o);
+            if (!Directory.Exists(FolderPath))
+            {
+                Directory.CreateDirectory(FolderPath);
+            }
+            File.WriteAllText(FilePath, jsonString, Encoding.UTF8);
+        }
+
+        public static T ReadJosn<T>(string path = "")
+        {
+            var jsonPath = !string.IsNullOrWhiteSpace(path) ? path : FilePath;
+            if (File.Exists(jsonPath))
+            {
+                string jsonString = File.ReadAllText(jsonPath);
+
+                return JsonConvert.DeserializeObject<T>(jsonString);
+            }
+            return default(T);
+        }
+    }
+}
