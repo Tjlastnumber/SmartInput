d8a8b914542080ffcd2361143ce5bb88189ef7b1
lastnumber <www.514163307@qq.com>
2018-12-05 14:08:37 +0800

    Update release.bat file update release.bat and add msbuild.bat help file

--- release.bat
+++ release.bat
@@ -1,7 +1,10 @@
 @echo off
+set OutName=SmartInput
 type %~dp0\Banner.txt
 @REM 需要配置本地环境变量 VS150COMNTOOLS 到vs的开发人员命令行目录
 call "%VS150COMNTOOLS%\VsDevCmd.bat"
-MSBuild %~dp0\SmartIME.sln /t:Rebuild /p:Configuration=Release
-echo Success
+MSBuild %~dp0\SmartIME.sln /t:Rebuild /p:Configuration=Release;AssemblyName=%OutName% /fl1
+echo Release Success
+@REM 生成成功后启动程序
+%~dp0\SmartInput\bin\Release\%OutName%
 pause
