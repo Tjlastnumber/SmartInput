de3373aabd144c9f75010c3cd451de0e448aa0d3
lastnumber <www.514163307@qq.com>
2018-12-05 11:55:46 +0800

    Update bat file
    update build.bat and release.bat

--- release.bat
+++ release.bat
@@ -2,6 +2,6 @@
 type %~dp0\Banner.txt
 @REM 需要配置本地环境变量 VS150COMNTOOLS 到vs的开发人员命令行目录
 call "%VS150COMNTOOLS%\VsDevCmd.bat"
-MSBuild %~dp0\SmartIME.sln /t:Rebuild
+MSBuild %~dp0\SmartIME.sln /t:Rebuild /p:Configuration=Release
 echo Success
 pause
